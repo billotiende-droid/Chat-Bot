f8a45a43cb26a2b19aac7fdc729ff30971f31c07
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-10-29 12:33:46 +0300

    components-chat-bot-structuring-props

---
+++ src/ChatInput.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function ChatInput ()  {
+
+return(
+    <div>
+        <input placeholder='send a message to chat bot' 
+        size="30" />
+        
+        <button>Send</button>
+
+    </div>
+
+)
+
+
+}
+
+
+export default ChatInput;
--- src/ChatMessage.jsx
+++ src/ChatMessage.jsx
@@ -1,12 +1,26 @@
 
 
 
-function ChatMessage  ({message}) {
+function ChatMessage  ({message,sender}) {
+
+// if(sender === 'robot') {
+//     return (
+//         <div>
+//             {message}
+//             <img src="src/images/bot.png" alt="Robot-avatar" width="90"/>
+//         </div>
+//     )
+// }
 
     return (
         <div>
+            {sender==='robot' && (
+            <img src="src/images/bot.png" alt="Robot-avatar" width="70"/>  // short form for if conditional rendering
+            )}
             {message}
-            <img src="src/images/user.png" alt="User-avatar" width="90"/>
+            {sender==='user' && (
+            <img  src="src/images/user.png" alt="User-avatar" width="50"/>
+            )}
 
         </div>
     )
