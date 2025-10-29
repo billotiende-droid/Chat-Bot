ca7b2a3de2c1a6f45d872e5d763c2eed0767aab0
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-10-29 11:48:47 +0300

    populate-chat

---
+++ src/ChatMessage.jsx
@@ -0,0 +1,16 @@
+
+
+
+function ChatMessage  ({message}) {
+
+    return (
+        <div>
+            {message}
+            <img src="src/images/user.png" alt="User-avatar" width="90"/>
+
+        </div>
+    )
+
+}
+
+export default ChatMessage;
